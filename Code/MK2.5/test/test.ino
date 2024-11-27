b5a8395d0be146e40eab352b827c5471496a572f
jamiejcole <jamiejcole@icloud.com>
2024-11-27 14:05:10 +1100

    fixed mk2.5 ish

--- Code/MK2.5/test/test.ino
+++ Code/MK2.5/test/test.ino
@@ -21,17 +21,17 @@ SLIPEncodedSerial SLIPSerial(Serial);
 #endif
 
 #define NUM_BUTTONS 4
-#define MCP_PIN_MX1_SHIFT 7
-#define MCP_PIN_MX2_GO 6
-#define MCP_PIN_MX3_STOP 5
-#define MCP_PIN_MX4_PAGE 4
-const int LED_R = 5;
-const int LED_G = 6;
-const int LED_B = 7;
-
+#define MCP_PIN_MX1_SHIFT 4
+#define MCP_PIN_MX2_GO 5
+#define MCP_PIN_MX3_STOP 6
+#define MCP_PIN_MX4_PAGE 7
 #define OSC_BUF_MAX_SIZE 512
 #define PING_AFTER_IDLE_INTERVAL 2500
 #define TIMEOUT_AFTER_IDLE_INTERVAL 5000
+
+const int LED_R = 14;
+const int LED_G = 12;
+const int LED_B = 13;
 
 const uint8_t BUTTON_PINS[NUM_BUTTONS] = {
   MCP_PIN_MX1_SHIFT,
@@ -42,11 +42,12 @@ const uint8_t BUTTON_PINS[NUM_BUTTONS] = {
 
 const char* encoderFunctions[][4] = {
   {"pan", "tilt", "zoom", "edge"}, // page 1
-  {"red", "green", "blue", "white"} // page 2
+  {"red", "green", "blue", "iris"} // page 2
 };
 
 int currentPage = 0;
 const int TICK_AMOUNT = 2;
+int shiftState = 0;
 
 enum ConsoleType
 {
@@ -72,7 +73,8 @@ void readyBlink();
 void parseOscMessage(String &msg);
 void issueEosSubscribes();
 void activatePage(int page);
-void sendKeyPress(const String &key);
+void sendKeyPress(const String &key, int shiftMod = -1);
+void writeLED(int r, int g, int b);
 
 RotaryEncOverMCP rotaryEncoders[] = {
     RotaryEncOverMCP(&mcp, 8, 9, &RotaryEncoderChanged, 1),
@@ -90,34 +92,37 @@ void RotaryEncoderChanged(bool clockwise, int id) {
   sendEncoderMovement(currentPage, id - 1, (clockwise ? -TICK_AMOUNT : TICK_AMOUNT));
 }
 
+void writeLED(int r, int g, int b) {
+  analogWrite(LED_R, r);
+  analogWrite(LED_G, g);
+  analogWrite(LED_B, b);
+}
+
 void readyBlink() {
   analogWrite(LED_R,   0);
   analogWrite(LED_G, 255);
   analogWrite(LED_B,  0);
-  delay(200);
+  delay(350);
   analogWrite(LED_G, 0);
-  delay(200);
+  delay(100);
   analogWrite(LED_G, 255);
-  delay(200);
-  analogWrite(LED_G, 255);
+  delay(350);
+  analogWrite(LED_G, 0);
 }
 
 void startupBlink() {
-  analogWrite(LED_R,   255);
-  analogWrite(LED_G, 0);
-  analogWrite(LED_B,  0);
-  delay(200);
-  analogWrite(LED_R,   0);
-  analogWrite(LED_G, 255);
-  analogWrite(LED_B,  0);
-  delay(200);
-  analogWrite(LED_R,   0);
-  analogWrite(LED_G, 0);
-  analogWrite(LED_B,  255);
-  delay(200);
-  analogWrite(LED_R,   0);
-  analogWrite(LED_G, 0);
-  analogWrite(LED_B,  0);
+  writeLED(255, 0, 0);
+  delay(500);
+  writeLED(0, 0, 0);
+  delay(100);
+  writeLED(0, 255, 0);
+  delay(500);
+  writeLED(0, 0, 0);
+  delay(100);
+  writeLED(0, 0, 255);
+  delay(500);
+  writeLED(0, 0, 0);
+  delay(100);
 }
 
 void waitingForConnectionBlink() {
@@ -150,12 +155,17 @@ void sendOscMessage(const String &address, float value) {
 void sendEncoderMovement(int page, int encoder, float ticks) {
   // we want to send a string e.g. /eos/wheel/coarse/pan
   float sentTicks = ticks;
-  String wheelMsg("/eos/wheel");
-  wheelMsg.concat("/coarse/");
-  wheelMsg.concat(encoderFunctions[page][encoder]);
-  if (page == 0 && encoder == 3) // double ticks for intensity
-    sentTicks = 5;
-  sendOscMessage(wheelMsg, sentTicks);
+  // if (page == 0 && encoder == 3) { 
+  //   sentTicks *= 2; // double ticks for intensity
+  //   String wheelMsg("/eos/wheel/1/level");
+  //   sendOscMessage(wheelMsg, sentTicks);
+  // }
+  // else {
+    String wheelMsg("/eos/wheel");
+    wheelMsg.concat("/coarse/");
+    wheelMsg.concat(encoderFunctions[page][encoder]);
+    sendOscMessage(wheelMsg, sentTicks);
+  // }
 }
 
 void issueEosSubscribes() {
@@ -183,15 +193,11 @@ void parseOscMessage(String &msg) {
 
 void activatePage(int page) {
   if (page == 0) {
-    analogWrite(LED_R, 200);
-    analogWrite(LED_G, 255);
-    analogWrite(LED_B, 0);
+    writeLED(186, 61, 34);
     currentPage = 0;
   }
   else if (page == 1) {
-    analogWrite(LED_R, 212);
-    analogWrite(LED_G, 0);
-    analogWrite(LED_B, 255);
+    writeLED(34, 186, 148);
     currentPage = 1;
   }
   else {
@@ -199,16 +205,26 @@ void activatePage(int page) {
   }
 }
 
-void sendKeyPress(const String &key) {
+void sendKeyPress(const String &key, int shiftMod) {
   String msg = "/eos/key/" + key;
-  OSCMessage keyMsg(msg.c_str());
-  SLIPSerial.beginPacket();
-  keyMsg.send(SLIPSerial);
-  SLIPSerial.endPacket();
+  if (shiftMod == 1) {
+    sendOscMessage(msg, 1.0);
+    shiftState = 1;
+  }
+  else if (shiftMod == 0) {
+    sendOscMessage(msg, 0.0);
+    shiftState = 0;
+  }
+  else {
+    OSCMessage keyMsg(msg.c_str());
+    SLIPSerial.beginPacket();
+    keyMsg.send(SLIPSerial);
+    SLIPSerial.endPacket();
+  }
 }
 
 void setup(){
-  delay(500); // Give time for bootup to finish
+  delay(2000); // Give time for bootup to finish
 
   Wire.begin();
   Wire.setClock(400000);
@@ -247,9 +263,6 @@ void setup(){
   SLIPSerial.beginPacket();
   SLIPSerial.write((const uint8_t*)HANDSHAKE_REPLY.c_str(), (size_t)HANDSHAKE_REPLY.length());
   SLIPSerial.endPacket();
-
-  Serial.begin(115200);
-  Serial.println("test");
 
   issueEosSubscribes();
 
@@ -313,22 +326,31 @@ void loop() {
   }
 
   for (int i = 0; i < NUM_BUTTONS; i++) {
-    if (i > 0) {
-      if (buttons[i].fell()) {
-        if (!buttons[0].read())
-          Serial.print("SHIFT + ");
-        Serial.print("Button ");
-        Serial.print(i+1);
-        Serial.println(" was pushed.");
-        if (i == 1)
-          sendKeyPress("go_0");
-        if (i == 2)
-          sendKeyPress("stop");
-        if (i == 3) {// Page button
-          if (currentPage == 1)
-            activatePage(0);
-          else if (currentPage == 0)
-            activatePage(1);
+    if (i == 0 && buttons[i].rose())
+      sendKeyPress("shift", 0); //0
+    if (i == 0 && buttons[i].fell()) {
+      sendKeyPress("shift", 1); //1
+    }
+    if (buttons[i].fell()) {
+      if (i == 1)
+        sendKeyPress("go_0");
+      if (i == 2)
+        sendKeyPress("stop");
+      if (i == 3) {
+        Serial.println();
+        Serial.print("Shift state: ");
+        Serial.println(shiftState);
+        if (currentPage == 1) {
+          activatePage(0);
+        }
+        else if (currentPage == 0) {
+          activatePage(1);
+        }
+        if (shiftState == 1) {
+          shiftState = 0;
+          Serial.println("RESTARTING!!!");
+          delay(100);
+          ESP.restart();
         }
       }
     }
